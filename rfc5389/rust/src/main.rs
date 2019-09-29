ff37472e418ab209300344b785267319cb587f17
matsub <matsub.rk@gmail.com>
2019-09-29 16:09:53 +0900

    recv and dump udp packet

--- rfc5389/rust/src/main.rs
+++ rfc5389/rust/src/main.rs
@@ -1,3 +1,36 @@
+use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::io;
+
+fn recv()  -> Result<(), io::Error> {
+    // Define the local connection information
+    let ip = Ipv4Addr::new(0, 0, 0, 0);
+    let connection = SocketAddrV4::new(ip, 3478);
+
+    // Bind the socket
+    let socket = UdpSocket::bind(connection)?;
+
+    // Read from the socket
+    let mut buf = [0; 1500];
+    let (amt, _) = socket.recv_from(&mut buf)?;
+
+    // Print only the valid data (slice)
+    let mut i = 0;
+    for b in &buf[0 .. amt] {
+        print!("{:02x}", b);
+        if i%2 == 1 {
+            print!(" ");
+        }
+        i += 1;
+    }
+
+    println!("");
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match recv() {
+        Ok(()) => {},
+        Err(err) => println!("Error: {:?}", err),
+    }
 }
